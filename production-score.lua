cac25fb12c630c7eeb83e4a452513dd3e5441310
randomdud3 <johnwrl13@gmail.com>
2019-03-24 21:21:31 +0100

    Fix the energy value for recipes with more than 1 unit of output

--- production-score.lua
+++ production-score.lua
@@ -45,7 +45,7 @@ local function get_product_list()
         for j, ingredient in pairs (ingredients) do
           recipe_ingredients[ingredient.name] = ((ingredient.amount)/#products) / product_amount
         end
-        recipe_ingredients.energy = recipe_prototype.energy
+        recipe_ingredients.energy = recipe_prototype.energy / #products / product_amount
         table.insert(product_list[product.name], recipe_ingredients)
       end
     end
